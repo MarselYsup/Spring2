4d1d8d079ccc1f977e6853ec03ccb71dcaff8bc4
MarselYsup <artem.dudin.1975@mail.ru>
2022-10-02 13:18:56 +0300

    make unit and integration tests

--- app/src/main/java/com/edu/ulab/app/repository/BookRepository.java
+++ app/src/main/java/com/edu/ulab/app/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package com.edu.ulab.app.repository;
 
 import com.edu.ulab.app.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,11 +10,12 @@ import javax.persistence.LockModeType;
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("select b from Book b where b.id = :id")
     Optional<Book> findByIdForUpdate(long id);
 
+    @Query("select b from Book b where b.person.id = :userId")
     List<Book> findAllByUserId(Long userId);
 }
